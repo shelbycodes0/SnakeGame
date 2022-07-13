4ec7c7d8315b1b638764b824fd6feeec9a3f8c66
Shelby Courtney <shelbycodes@gmail.com>
2022-07-12 20:30:18 -0400

    updated boundary coordinates
    so it looks more like the snake actually hits the wall instead of stopping the game before he collides

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ while game_is_on:
         score.increase_score()
 
 #### detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         score.game_over()
 
