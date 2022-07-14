b78899fdc1bbc7c29945e827bf4d0d4e18b638ae
Shelby Courtney <shelbycodes@gmail.com>
2022-07-14 14:48:00 -0400

    updated scoreboard to include high score
    created new text file to save high score input and update ever time program is played

--- main.py
+++ main.py
@@ -39,14 +39,14 @@ while game_is_on:
 
 #### detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
 #### detect collision with tail
     for tail in snake.segments[1:]:
         if snake.head.distance(tail) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
 
 
